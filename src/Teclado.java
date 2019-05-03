85108751d318270800ab19603cc8bb3c1ff346d0
abel3105 <49993461+abel3105@users.noreply.github.com>
2019-05-03 13:12:55 -0500

    Javalearn

---
+++ src/Teclado.java
@@ -0,0 +1,14 @@
+public class Teclado {
+
+	public static void main(String[] args) {
+	
+	int numero=100;
+    System.out.println( "Hello World!");
+
+    do {
+    	System.out.println("los numeros son"+numero);
+    numero=numero+1;}
+    while(numero<200);
+
+	}
+}
